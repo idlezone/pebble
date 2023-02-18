bc107ec50818fa0730c537da155581f13a3233e8
Idlezone <idlesession@gmail.com>
2023-02-18 18:03:48 +0800

    use chrome adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "sveltekit-adapter-chrome-extension";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		// adapter: adapter(),
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: null,
+			precompress: false,
+			manifest: "manifest.json",
+			emptyOutDir: true,
+		}),
+		appDir: 'app'
 	}
 };
 
